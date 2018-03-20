32a0a7d67ca855e3c056221d05be9f76770ebb2f
cmonte905 <cmonte905@hotmail.com>
2018-03-20 01:35:58 +0000

    Added gitigore, started basic express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,17 @@
-const express = require():
-console.log('hello')
-console.log('world')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world, this is status'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
